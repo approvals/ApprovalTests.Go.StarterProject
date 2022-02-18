bb8a3e8446b4353cd612231e9dd99f8e9e3d24d5
Llewellyn Falco <llewellyn.falco@gmail.com>
2022-02-18 02:23:24 +0000

    t First Approval test
    Co-Authored-By: Sudarshan Gaikaiwari <sudarshan@acm.org>

--- sample_test.go
+++ sample_test.go
@@ -1,9 +1,16 @@
 package starterpackage
 
-import "testing"
+import (
+	approvals "github.com/approvals/go-approval-tests"
+	"testing"
+)
 
 func TestNormalTest(t *testing.T) {
 	if 5 != 5 {
 		t.Fatal("5 != 5")
 	}
+}
+
+func TestBasicApproval(t *testing.T) {
+	approvals.VerifyString(t, "Hello Approvals")
 }
